3476eb387d078042ce8a1bf63b79d3c884d3c257
SnippyRO <81987817+SnippyRP@users.noreply.github.com>
2021-10-10 19:40:44 -0700

    Update content.lua

--- content.lua
+++ content.lua
@@ -3,6 +3,37 @@ local Universus = {}
 
 local TweenService = game:GetService("TweenService")
 local Players = game:GetService("Players")
+local Mouse = game.Players.LocalPlayer:GetMouse()
+
+function CircleClick(Button, X, Y)
+	coroutine.resume(coroutine.create(function()
+		
+		Button.ClipsDescendants = true
+		
+		local Circle = script:WaitForChild("Circle"):Clone()
+			Circle.Parent = Button
+			local NewX = X - Circle.AbsolutePosition.X
+			local NewY = Y - Circle.AbsolutePosition.Y
+			Circle.Position = UDim2.new(0, NewX, 0, NewY)
+		
+		local Size = 0
+			if Button.AbsoluteSize.X > Button.AbsoluteSize.Y then
+				 Size = Button.AbsoluteSize.X*1.5
+			elseif Button.AbsoluteSize.X < Button.AbsoluteSize.Y then
+				 Size = Button.AbsoluteSize.Y*1.5
+			elseif Button.AbsoluteSize.X == Button.AbsoluteSize.Y then																										Size = Button.AbsoluteSize.X*1.5
+			end
+		
+		local Time = 0.5
+			Circle:TweenSizeAndPosition(UDim2.new(0, Size, 0, Size), UDim2.new(0.5, -Size/2, 0.5, -Size/2), "Out", "Quad", Time, false, nil)
+			for i=1,10 do
+				Circle.ImageTransparency = Circle.ImageTransparency + 0.01
+				wait(Time/10)
+			end
+			Circle:Destroy()
+			
+	end))
+end
 
 local MainGui = Instance.new("ScreenGui")
 local MainFrame = Instance.new("Frame")
@@ -110,6 +141,7 @@ end)
 end)
   
   TextButton.MouseButton1Click:Connect(function()
+	CircleClick(TextButton, Mouse.X, Mouse.Y) 
       callback()
    end)
 
