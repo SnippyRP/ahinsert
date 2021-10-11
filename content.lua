e416397168d169973451614078906d186a867f83
SnippyRO <81987817+SnippyRP@users.noreply.github.com>
2021-10-10 18:50:13 -0700

    Update content.lua

--- content.lua
+++ content.lua
@@ -59,8 +59,8 @@ UIPadding.Parent = Menu
 UIPadding.PaddingLeft = UDim.new(0, 10)
 UIPadding.PaddingTop = UDim.new(0, 10)
 
-function Universus:Prt(name)
-  print(name)
+function Universus:Prt(name,callback)
+  callback = callback or function() end
 local Button = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
 local TextButton = Instance.new("TextButton")
@@ -97,6 +97,26 @@ UIGradient.Parent = TextButton
 
 UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(96, 202, 249)), ColorSequenceKeypoint.new(0.24, Color3.fromRGB(197, 164, 251)), ColorSequenceKeypoint.new(0.52, Color3.fromRGB(223, 157, 255)), ColorSequenceKeypoint.new(0.77, Color3.fromRGB(207, 182, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(183, 227, 233))}
 UIGradient_2.Parent = Button
+  
+  TextButton.MouseEnter:Connect(function()
+	local tween = TweenService:Create(UIGradient_2, TweenInfo.new(.25), {["Rotation"] = 180});tween:Play()
+	local tween = TweenService:Create(Button, TweenInfo.new(.25), {["BackgroundColor3"] = Color3.fromRGB(175, 186, 170)});tween:Play()
+end)
+  
+  TextButton.MouseLeave:Connect(function()
+	local tween = TweenService:Create(UIGradient_2, TweenInfo.new(.25), {["Rotation"] = 0});tween:Play()
+	local tween = TweenService:Create(Button, TweenInfo.new(.25), {["BackgroundColor3"] = Color3.fromRGB(98, 104, 95)});tween:Play()
+end)
+  
+TextButton.Activated:Connect(function()
+	local tween = TweenService:Create(Button, TweenInfo.new(.1), {["Size"] = UDim2.new(0.613, 0,0.097, 0),["Position"] = UDim2.new(0.175, 0,0.451, 0)});tween:Play()
+	wait(.1)
+	local tween = TweenService:Create(Button, TweenInfo.new(.1), {["Size"] = UDim2.new(0.694, 0,0.139, 0),["Position"] = UDim2.new(0.136, 0,0.429, 0)});tween:Play()
+end)
+  
+  TextButton.MouseButton1Click:Connect(function()
+      callback()
+   end)
 
 end
 return Universus
