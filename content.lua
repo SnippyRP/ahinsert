606df032111d259ad8d7a505fe6364c52697a081
SnippyRO <81987817+SnippyRP@users.noreply.github.com>
2021-10-10 17:59:06 -0700

    Update content.lua

--- content.lua
+++ content.lua
@@ -1,1 +1,13 @@
+print("Init")
+local Kavo = {}
 
+local tween = game:GetService("TweenService")
+local tweeninfo = TweenInfo.new
+local input = game:GetService("UserInputService")
+local run = game:GetService("RunService")
+
+local Utility = {}
+local Objects = {}
+function Kavo:Prt(frame, parent)
+  print("DRAG!")
+return Kavo
